eb2e255a8108cbe3616eb5a2e9a4a745efcd0f9c
Denis Cappellin <denis@cappell.in>
2022-04-30 10:57:49 +0200

    Add script to generate posts

---
+++ _scripts/postgen.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -t|--template)
+    TEMPLATE="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [ $# -eq 0 ]
+  then
+    echo "No argument(s) supplied. Provide the title of the note."
+    exit 1
+fi
+
+POSTPATH="../_posts/"
+EXTENSION=".markdown"
+FILENAME="`date +%Y-%m-%d ; echo`_$1${EXTENSION}"
+
+touch "${POSTPATH}${FILENAME}"
+
+if [ -z "$TEMPLATE" ]; then
+	echo -e "# $1" > ${POSTPATH}${FILENAME}
+else
+	cp -f "${TEMPLATE}" "${POSTPATH}${FILENAME}"
+fi
+
+echo "File ${FILENAME} created."
---
+++ _scripts/templates/speedhike.markdown
@@ -0,0 +1,21 @@
+---
+layout: post
+title:  ""
+date:   
+categories: speed-hiking
+---
+
+Concludo il percorso in  h  min. -  km -  D+
+
+Il [percorso][komoot] sul mio profilo [Koomot][komoot-profile]. Ti invito a visitarlo e se ti piacciono i miei “highlight” di escursionismo e corsa ricordati di mettere un “like” 👍. 
+
+Il [video][youtube] sul mio canale [YouTube][youtube-channel]. Ti invito a visitarlo. Se ti piacciano i miei video ricordati di mettere un “like” 👍 e iscriviti per non perdere tutte le prossime avventure.
+
+
+
+Alla prossima e buon giro!
+
+[komoot-profile]: https://www.komoot.it/user/1971346940863
+[komoot]: 
+[youtube-channel]: https://bit.ly/3jKVu80
+[youtube]: 
